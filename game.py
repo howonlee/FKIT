1ebbe62cc66febaeb6d699d057dbdeeed6a7c76a
Howon Lee <hlee.howon@gmail.com>
2013-01-12 16:51:49 -0800

    game

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import cv2, pygame, sys
+import cv2.cv as cv
 from pygame.locals import *
 from numpy import *
 
@@ -20,15 +21,29 @@ def draw_flow(im, flow, step=16):
 	return vis
 
 if __name__ == '__main__':
+
+	#webcam init
 	cap = cv2.VideoCapture(0)
 	ret, im = cap.read()
 	prev_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+	#pygame init
+	pygame.init()
+	fpsClock = pygame.time.Clock()
+	windowSurfaceObj = pygame.display.set_mode((im.shape[0], im.shape[1]))
+	pygame.display.set_caption("FKIT")
 
 	while True:
 		ret, im = cap.read()
 		gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 		flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, 0.5, 1, 3, 1, 3, 5, 1)
 		prev_gray = gray
-		cv2.imshow('Optical Flow', draw_flow(gray, flow))
-		if cv2.waitKey(10) == 27:
-			break
+		vis = cv.fromarray(draw_flow(gray, flow))
+		vis_rgb = cv.CreateMat(vis.height, vis.width, cv.CV_8UC3)
+		cv.CvtColor(vis, vis_rgb, cv.CV_BGR2RGB)
+		pgVis = pygame.image.frombuffer(vis_rgb.tostring(), cv.GetSize(vis_rgb), "RGB")
+		windowSurfaceObj.blit(pgVis, (0,0))
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
