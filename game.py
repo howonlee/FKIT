b7a24487cf2776d76a73b64cc5da4a2ec3b8bcd0
Howon Lee <hlee.howon@gmail.com>
2013-01-12 16:17:11 -0800

    parameter tuning

--- game.py
+++ game.py
@@ -14,9 +14,9 @@ def draw_flow(im, flow, step=16):
 	#vis = cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
 	vis = zeros((im.shape[0], im.shape[1], 3))
 	for (x1, y1), (x2, y2) in lines:
-		cv2.line(vis, (x1, y1), (x2, y2), (0, 255, 0), 1)
-		cv2.circle(vis, (x1, y1), 1, (0,255,0), -1)
-	print vis
+		cv2.line(vis, (x1, y1), (x2, y2), (255, 255, 255), 1)
+		cv2.circle(vis, (x1, y1), 1, (255,255,255), -1)
+	#print vis
 	return vis
 
 cap = cv2.VideoCapture(0)
@@ -27,7 +27,7 @@ prev_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 while True:
 	ret, im = cap.read()
 	gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-	flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, 0.5, 1, 3, 3, 3, 5, 1)
+	flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, 0.5, 1, 3, 1, 3, 5, 1)
 	prev_gray = gray
 	cv2.imshow('Optical Flow', draw_flow(gray, flow))
 	if cv2.waitKey(10) == 27:
